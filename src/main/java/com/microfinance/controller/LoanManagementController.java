b6bf501c67d248e32d1838bce95ae4eefd7dcfbd
Shraddha8680 <shraddhajambhulkar7@gmail.com>
2025-06-16 18:10:01 +0530

    Shraddha Changes 16/06/25

--- src/main/java/com/microfinance/controller/LoanManagementController.java
+++ src/main/java/com/microfinance/controller/LoanManagementController.java
@@ -1,35 +1,28 @@
 package com.microfinance.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microfinance.model.LoanSchemCatalog;
 import com.microfinance.service.LoanManagementService;
 
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
-
 @RestController
 public class LoanManagementController {
-	
+
 	@Autowired
-	private LoanManagementService loanServices ;
-	
-     @ResponseBody
+	private LoanManagementService loanServices;
+
+	// Shraddha - 16/06/2025
+	@ResponseBody
 	@PostMapping("/SaveLoan")
-	public String saveLoan(@RequestBody LoanSchemCatalog lone ) {
-		//TODO: process POST request
+	public String saveLoan(@RequestBody LoanSchemCatalog lone) {
+		// TODO: process POST request
 		loanServices.saveLoan(lone);
 		return "Save successfully";
 	}
-     
-     
-    
-	
 
 }
