9092b968d6a0432f62b2719df69be53289bdc4ed
Akansha Indurkar <akanshaindurkar6@gmail.com>
2025-06-17 18:18:39 +0530

    Delete target directory

--- target/classes/static/js/incentive/generateIncentive.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-		// Get today's date in YYYY-MM-DD format
-		let today = new Date().toISOString().split('T')[0];
-		// Set it to the input field
-		$("#dateFrom").val(today);
-		$("#dateTo").val(today);
-	});
