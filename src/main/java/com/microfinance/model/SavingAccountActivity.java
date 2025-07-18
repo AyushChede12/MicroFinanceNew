7e436f8c92aea3fa3f331707cb6c65dccb5b25fe
Janvi Sonkusare <janvisonkusare3@gmail.com>
2025-07-18 11:44:48 +0530

    janvi create saving acc/ saving acc activity changes

--- src/main/java/com/microfinance/model/SavingAccountActivity.java
+++ src/main/java/com/microfinance/model/SavingAccountActivity.java
@@ -24,6 +24,13 @@ public class SavingAccountActivity {
 	private String transactionType;
 	private String transactionAmount;
 	private String payBy;
+	private String chequeNo;
+    private String chequeDate;
+    private String depositAcc1;
+    private String depositAcc2;
+    private String refNumber1;
+    private String depositAcc3;
+    private String refNumber2;
 	
 	
 	public Long getId() {
@@ -121,6 +128,48 @@ public class SavingAccountActivity {
 	}
 	public void setPayBy(String payBy) {
 		this.payBy = payBy;
+	}
+	public String getChequeNo() {
+		return chequeNo;
+	}
+	public void setChequeNo(String chequeNo) {
+		this.chequeNo = chequeNo;
+	}
+	public String getChequeDate() {
+		return chequeDate;
+	}
+	public void setChequeDate(String chequeDate) {
+		this.chequeDate = chequeDate;
+	}
+	public String getDepositAcc1() {
+		return depositAcc1;
+	}
+	public void setDepositAcc1(String depositAcc1) {
+		this.depositAcc1 = depositAcc1;
+	}
+	public String getDepositAcc2() {
+		return depositAcc2;
+	}
+	public void setDepositAcc2(String depositAcc2) {
+		this.depositAcc2 = depositAcc2;
+	}
+	public String getRefNumber1() {
+		return refNumber1;
+	}
+	public void setRefNumber1(String refNumber1) {
+		this.refNumber1 = refNumber1;
+	}
+	public String getDepositAcc3() {
+		return depositAcc3;
+	}
+	public void setDepositAcc3(String depositAcc3) {
+		this.depositAcc3 = depositAcc3;
+	}
+	public String getRefNumber2() {
+		return refNumber2;
+	}
+	public void setRefNumber2(String refNumber2) {
+		this.refNumber2 = refNumber2;
 	}
 	
 	
