5a2b9aef6c87a0353349a583c8634adc3859e3f0
Janvi Sonkusare <janvisonkusare3@gmail.com>
2025-07-21 20:03:24 +0530

    janvi completed fund transfer

--- src/main/java/com/microfinance/repository/SavingAccountFundTransferRepo.java
+++ src/main/java/com/microfinance/repository/SavingAccountFundTransferRepo.java
@@ -1,9 +1,13 @@
 package com.microfinance.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.microfinance.model.savingAccountFundTransfer;
 
 public interface SavingAccountFundTransferRepo extends JpaRepository<savingAccountFundTransfer, Long>{
+
+	List<savingAccountFundTransfer> findByDebitAccountNumberAndIsApproved(String debitAccountNo, boolean b);
 
 }
