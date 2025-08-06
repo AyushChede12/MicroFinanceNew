95a41af8469db508ec3dec447e08181f9c311793
Janvi Sonkusare <janvisonkusare3@gmail.com>
2025-08-06 20:40:56 +0530

    janvi customer savings statement done

--- src/main/java/com/microfinance/repository/CreateSavingAccountRepo.java
+++ src/main/java/com/microfinance/repository/CreateSavingAccountRepo.java
@@ -31,6 +31,7 @@ public interface CreateSavingAccountRepo extends JpaRepository<CreateSavingsAcco
 	List<CreateSavingsAccount> findBySelectByCustomer(String selectByCustomer);
 
 	boolean existsBySelectByCustomer(String customerId);
+
 	
 
 	//CreateSavingsAccount findByaAccountNumber(String accountNumber);
